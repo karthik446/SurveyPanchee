4d4fa47b05e460908f8c7afb5bd2fe5ea6cb3871
dheerajduggiraju <dheerajduggiraju@gmail.com>
2016-04-19 12:45:33 -0500

    Created Angular Spa app
    creating app using yo angular generator

---
+++ WebApp/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spwebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the spwebApp
+ */
+angular.module('spwebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ WebApp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spwebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spwebApp
+ */
+angular.module('spwebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
